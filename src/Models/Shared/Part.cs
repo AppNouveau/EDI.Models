177c04362d16d57d2e9317d57c5b75433329a990
citems <gprestia@mnsi.net>
2018-05-23 14:43:55 -0400

    Added ReferenceId and PurchaseOrderNo to the parts class

--- src/Models/Shared/Part.cs
+++ src/Models/Shared/Part.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 namespace EDI.Models.Shared
 {
     /// <summary>
-    /// OID or AT8 (for stops) or L3 (for total)              
+    /// OID or AT8 (for stops) or L3 (for total)       
     /// </summary>
     public class Part
     {
+        public string ReferenceId { get; set; }
+        public string PurchaseOrderNo { get; set; }
         public double Weight { get; set; }
         public double Quantity { get; set; }
         public WeightQualifierType WeightQualifier { get; set; }
